161720a038cd9f7b1dfd94ddf781436142d348ef
sachin borge <borgedadaso50@gmail.com>
2018-12-31 11:27:17 +0530

    index.php added

--- index.php
+++ index.php
@@ -12,32 +12,27 @@
     <div class="wrapper">
       <section class="main_background">
         <header class="header-style fixed-top">
-			<div class="container">
-				<div class="header d-lg-flex justify-content-between align-items-center">
-					<!-- logo -->
-					<h1 class="logo text-center">
-
-					</h1>
-					<!-- //logo -->
-					<!-- nav -->
-					<div class="nav_w3ls">
-						<nav>
-							<label for="drop" class="toggle toogle-2">Menu</label>
-							<input type="checkbox" id="drop">
-							<ul class="menu">
-								<li class="active"><a href="index.html">About us</a></li>
-								<li><a href="#about">Work</a></li>
-                <li>	<a href="index.html"><img src="images/logo.png" alt="logo" title="logo">
-      						</a></li>
-								<li><a href="#blog">Shop</a></li>
-								<li><a href="#gallery">Contact</a></li>
-							</ul>
-						</nav>
-					</div>
-					<!-- //nav -->
-				</div>
-			</div>
-		</header>
+      			<div class="container">
+      				<div class="header d-lg-flex justify-content-between align-items-center">
+      					<!-- nav -->
+      					<div class="nav_w3ls">
+      						<nav>
+      							<label for="drop" class="toggle toogle-2">Menu</label>
+      							<input type="checkbox" id="drop">
+      							<ul class="menu">
+      								<li class="active"><a href="index.html">About us</a></li>
+      								<li><a href="#about">Work</a></li>
+                      <li>	<a href="index.html"><img src="images/logo.png" alt="logo" title="logo">
+            						</a></li>
+      								<li><a href="#blog">Shop</a></li>
+      								<li><a href="#gallery">Contact</a></li>
+      							</ul>
+      						</nav>
+      					</div>
+      					<!-- //nav -->
+      				</div>
+      			</div>
+		      </header>
       </section><!--main background section-->
     </div><!--main wrapper-->
     <? php include 'footer.php';?>
