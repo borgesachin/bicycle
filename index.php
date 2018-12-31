fa5bbdf500a5e47e6afe665ae102ed34b5708c7e
sachin borge <borgedadaso50@gmail.com>
2018-12-31 11:25:14 +0530

    this is second commit

--- index.php
+++ index.php
@@ -1,15 +1,43 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Bicycle</title>
-    <link rel="stylesheet" href="css/css/style.css">
-  </head>
-  <body>
-    <!-- <? php include 'head.php';?> -->
+
+  <!-- <? php include 'head.php';?> -->
+  <!DOCTYPE html>
+  <html lang="en" dir="ltr">
+    <head>
+      <meta charset="utf-8">
+      <title>Bicycle</title>
+      <link rel="stylesheet" href="css/bootstrap.css">
+      <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
     <div class="wrapper">
       <section class="main_background">
+        <header class="header-style fixed-top">
+			<div class="container">
+				<div class="header d-lg-flex justify-content-between align-items-center">
+					<!-- logo -->
+					<h1 class="logo text-center">
 
+					</h1>
+					<!-- //logo -->
+					<!-- nav -->
+					<div class="nav_w3ls">
+						<nav>
+							<label for="drop" class="toggle toogle-2">Menu</label>
+							<input type="checkbox" id="drop">
+							<ul class="menu">
+								<li class="active"><a href="index.html">About us</a></li>
+								<li><a href="#about">Work</a></li>
+                <li>	<a href="index.html"><img src="images/logo.png" alt="logo" title="logo">
+      						</a></li>
+								<li><a href="#blog">Shop</a></li>
+								<li><a href="#gallery">Contact</a></li>
+							</ul>
+						</nav>
+					</div>
+					<!-- //nav -->
+				</div>
+			</div>
+		</header>
       </section><!--main background section-->
     </div><!--main wrapper-->
     <? php include 'footer.php';?>
