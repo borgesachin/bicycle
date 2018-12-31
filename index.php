ab4dfc2b4be13d0ca246f9c3fa36b2c5944e89ef
sachin borge <borgedadaso50@gmail.com>
2018-12-31 10:37:44 +0530

    this is first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Bicycle</title>
+    <link rel="stylesheet" href="css/css/style.css">
+  </head>
+  <body>
+    <!-- <? php include 'head.php';?> -->
+    <div class="wrapper">
+      <section class="main_background">
+
+      </section><!--main background section-->
+    </div><!--main wrapper-->
+    <? php include 'footer.php';?>
+  </body>
+</html>
